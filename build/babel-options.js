fce97ef267f349f63237227044ce83c57797bb15
Mike Graham <mgraham@technicalmedia.com>
2016-01-23 11:02:28 -0700

    chore(metadata): update build to babel6

--- build/babel-options.js
+++ build/babel-options.js
@@ -4,32 +4,23 @@ var paths = require('./paths');
 module.exports = {
   filename: '',
   filenameRelative: '',
-  modules: '',
   sourceMap: true,
-  sourceMapName: '',
   sourceRoot: '',
   moduleRoot: path.resolve('src').replace(/\\/g, '/'),
   moduleIds: false,
-  experimental: false,
   comments: false,
   compact: false,
   code:true,
-  stage:2,
-  loose: "all",
-  optional: [
-    "es7.decorators",
-    "es7.classProperties"
-  ],
+  presets: ['es2015', 'stage-2'],
   plugins: [
-    "babel-dts-generator"
-  ],
-  extra: {
-    dts: {
-      packageName: paths.packageName,
-      typings: '',
-      suppressModulePath: true,
-      suppressComments: false,
-      memberOutputFilter: /^_.*/
-    }
-  }
+    'syntax-flow',
+    'transform-decorators-legacy',
+    ['babel-dts-generator', {
+        packageName: paths.packageName,
+        typings: '',
+        suppressModulePath: true,
+        suppressComments: false,
+        memberOutputFilter: /^_.*/
+    }]
+  ]
 };
